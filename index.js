acb08c8a2cbf58a0576e88612192ab3d27a49f6b
Jose Gutierrez <51543641+pepenautamx@users.noreply.github.com>
2021-02-28 18:44:59 -0600

    Primer commit

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const cors = require('cors');
+// Importar Express
+const express = require('express');
+
+
+const { dbConnection } = require('./database/config');
+
+// Crear el servidor de express
+const app = express();
+
+// Configurar cors
+app.use(cors());
+
+//Base de datos
+dbConnection();
+
+// console.log(process.env);
+
+// Usuario: mean_user  Password: wHSmwH24vyik4nHs   
+
+//Rutas
+app.get('/', (req, res) => {
+
+    res.json({
+        ok: true,
+        msg: 'Hola Mundo'
+    });
+});
+
+//Levantar el servidor
+app.listen(process.env.PORT, () => {
+    console.log('Servidor corriendo en puerto  ' + process.env.PORT);
+});
